320004ca1fea7af0cfdde017aeba6d1f15a88cbf
Joao Alexandre <alexandre_efeta@hotmail.com>
2022-07-15 12:21:21 -0300

    Exercicio membros estaticos em C#

---
+++ C#/MembrosEstaticos/MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MembrosEstaticos
+{
+    class ConversorDeMoeda
+    {
+        public static double ConverterMoeda(double valorDolar, double qtdDolar) {
+            return valorDolar * qtdDolar + CalcularIOF(valorDolar,qtdDolar);
+        }
+
+        public static double CalcularIOF(double valorDolar, double qtdDolar) {
+            return valorDolar * qtdDolar * 6 / 100;
+        }
+
+    }
+}
---
+++ C#/MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("\nQuantos dólares você vai comprar? ");
+            double qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"\nValor a ser pago em reais = {ConversorDeMoeda.ConverterMoeda(valorDolar,qtdDolar).ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+        }
+    }
+}
